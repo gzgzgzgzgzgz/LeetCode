fdcd321c350bc07939b83f917c73feabb75c3d7a
Zhen Guan <52941906+gzgzgzgzgzgz@users.noreply.github.com>
2021-03-24 08:37:51 -0700

    Update swap-nodes-in-pairs.py

--- swap-nodes-in-pairs/swap-nodes-in-pairs.py
+++ swap-nodes-in-pairs/swap-nodes-in-pairs.py
@@ -5,18 +5,25 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: ListNode) -> ListNode:
+#         if not head or not head.next:
+#             return head
+#         dummy_node = ListNode(0,head)
+#         temp = dummy_node
+#         while (temp.next and temp.next.next):
+#             node1 = temp.next
+#             node2 = temp.next.next
+            
+#             node1.next = node2.next
+#             node2.next = node1
+#             temp.next = node2
+            
+#             temp = node1
+#         return dummy_node.next
+        #递归
         if not head or not head.next:
             return head
-        dummy_node = ListNode(0,head)
-        temp = dummy_node
-        while (temp.next and temp.next.next):
-            node1 = temp.next
-            node2 = temp.next.next
-            
-            node1.next = node2.next
-            node2.next = node1
-            temp.next = node2
-            
-            temp = node1
-        return dummy_node.next
+        newHead = head.next
+        head.next = self.swapPairs(newHead.next)
+        newHead.next = head
+        return newHead
             
